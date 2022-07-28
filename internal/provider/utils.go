5fe88a9923138decd39f8f8a76cb7b0504d53b90
Éric Falconnier <eric@zentral.pro>
2022-07-28 11:11:26 +0200

    Rename ID variable in import helper

--- internal/provider/utils.go
+++ internal/provider/utils.go
@@ -11,13 +11,13 @@ import (
 )
 
 func resourceImportStatePassthroughZentralID(ctx context.Context, name string, req tfsdk.ImportResourceStateRequest, resp *tfsdk.ImportResourceStateResponse) {
-	tagID, err := strconv.ParseInt(req.ID, 10, 64)
+	ztlID, err := strconv.ParseInt(req.ID, 10, 64)
 	if err != nil {
 		resp.Diagnostics.AddError(
 			"Invalid resource ID",
 			fmt.Sprintf("Zentral %s ID must be an integer", name),
 		)
 	} else {
-		resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), types.Int64{Value: tagID})...)
+		resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), types.Int64{Value: ztlID})...)
 	}
 }
